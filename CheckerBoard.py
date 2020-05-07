2d537ebb9664b0d71335c4b6d675d32c12e51d25
Nelson Wells <nelsonwellswku@gmail.com>
2020-05-07 16:29:19 -0500

    Draw checkers on the board

--- CheckerBoard.py
+++ CheckerBoard.py
@@ -1,11 +1,8 @@
-red = (255, 0, 0)
-green = (0, 255, 0)
-black = (255, 255, 255)
-white = (0, 0, 0)
+from Palette import Black, White, Red, Green
 
 
-def _generate_empty_row(color_one, color_two):
-    return [Square(color_one, None) if x % 2 != 0 else Square(color_two, None) for x in range(8)]
+def _generate_row(color_one, color_two, checker_one=None, checker_two=None):
+    return [Square(color_one, checker_one) if x % 2 == 0 else Square(color_two, checker_two) for x in range(8)]
 
 
 class Checker:
@@ -23,12 +20,12 @@ class Square:
 class CheckerBoard:
     def __init__(self):
         self.board = [
-            _generate_empty_row(white, black),
-            _generate_empty_row(black, white),
-            _generate_empty_row(white, black),
-            _generate_empty_row(black, white),
-            _generate_empty_row(white, black),
-            _generate_empty_row(black, white),
-            _generate_empty_row(white, black),
-            _generate_empty_row(black, white),
+            _generate_row(Black, White, Checker(Green)),
+            _generate_row(White, Black, None, Checker(Green)),
+            _generate_row(Black, White, Checker(Green)),
+            _generate_row(White, Black),
+            _generate_row(Black, White),
+            _generate_row(White, Black, None, Checker(Red)),
+            _generate_row(Black, White, Checker(Red)),
+            _generate_row(White, Black, None, Checker(Red))
         ]
